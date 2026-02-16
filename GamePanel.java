97afd67814cd1e6483e06d39453c35a4b6a15678
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-16 22:04:19 +0530

    Make maxScreenCol and maxScreenRow public; integrate TileManager drawing in paintComponent

--- GamePanel.java
+++ GamePanel.java
@@ -12,14 +12,15 @@ public class GamePanel extends JPanel implements Runnable {
     final int scale = 3;
     public final int tileSize = originalTileSize * scale; // 48x48 tile
 
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
     public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     // FPS
     int FPS = 60;
-
+    
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
@@ -93,6 +94,8 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
+
+        tileM.draw(g2);
 
         player.draw(g2);
 
