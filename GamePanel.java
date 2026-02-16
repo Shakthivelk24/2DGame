d6ec2a2b77d0e63118c8902ced0c52ced88786cc
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-16 21:40:14 +0530

    Remove default player position and speed variables from GamePanel

--- GamePanel.java
+++ GamePanel.java
@@ -24,10 +24,7 @@ public class GamePanel extends JPanel implements Runnable {
     Thread gameThread;
     Player player = new Player(this, keyH);
 
-    // Set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
