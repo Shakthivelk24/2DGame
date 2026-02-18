b9a6b91087fc7a8579ad83bca399406bb5d53713
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-18 21:33:51 +0530

    Add collision detection properties to Entity class

--- Entity.java
+++ Entity.java
@@ -1,3 +1,4 @@
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Entity {
@@ -10,5 +11,8 @@ public class Entity {
 
     public int spriteCounter = 0;
     public int spriteNum = 1;
+
+    public Rectangle solidArea ;
+    public boolean collisionOn = false;
 
 }
