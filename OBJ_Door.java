813357d19af4468e95ad079b7cae899e8005e7ac
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-19 21:05:30 +0530

    Add OBJ_Door class to represent door objects in the game

---
+++ OBJ_Door.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class OBJ_Door extends SuperObject {
+
+    public OBJ_Door() {
+        name = "Door";
+        collision = true;
+
+        try {
+            image = ImageIO.read(
+                getClass().getResourceAsStream("/Object/door.png")
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
