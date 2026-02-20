bcbd9c601b978a6a430b959f0b17212a8ea7f3e2
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-20 21:53:37 +0530

    Add solidArea and default solidArea fields to SuperObject class

--- SuperObject.java
+++ SuperObject.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class SuperObject {
@@ -7,6 +8,8 @@ public class SuperObject {
     public String name;
     public boolean collision = false;
     public int worldX, worldY;
+    public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+    public int solidAreaDefaultX = 0, solidAreaDefaultY = 0;
 
     public void draw(Graphics2D g2, GamePanel gp) {
         int screenX = worldX - gp.player.worldx + gp.player.screenX; 
