441301196749f9aeb37a9f2aa7ca6895cf98bbbd
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-25 18:29:39 +0530

    Refactor GamePanel to replace array with ArrayList for object management and improve iteration with foreach loop

--- GamePanel.java
+++ GamePanel.java
@@ -2,28 +2,29 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList; // 🔴 CHANGE: Collection import
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable {
-    // Screen Settings
-    final int originalTileSize = 16; // 16x16 tile final
 
+    // Screen Settings
+    final int originalTileSize = 16;
     final int scale = 3;
-    public final int tileSize = originalTileSize * scale; // 48x48 tile
+    public final int tileSize = originalTileSize * scale;
 
     public final int maxScreenCol = 16;
     public final int maxScreenRow = 12;
-    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
- 
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+
     // World Settings
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
 
     // FPS
     int FPS = 60;
-    
+
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Sound music = new Sound();
@@ -35,11 +36,9 @@ public class GamePanel extends JPanel implements Runnable {
 
     // Entity and Object
     public Player player = new Player(this, keyH);
-    public SuperObject obj[] = new SuperObject[10];
 
-
-
-
+    // 🔴 CHANGE: ARRAY → COLLECTION
+    public ArrayList<SuperObject> objList = new ArrayList<>();
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -60,45 +59,21 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     @Override
-    // Sleep method is not accurate, so we will use delta method to calculate the time
-    // public void run() {
-    // double drawInterval = 1000000000/FPS; // 0.01666 seconds
-    // double nextDrawTime = System.nanoTime() + drawInterval;
-
-    // while(gameThread != null) {
-    // // Update: update information such as character positions
-    // update();
-    // // Drew: draw the screen with the updated information
-    // repaint();
-
-    // try {
-    // double remainingTime = nextDrawTime - System.nanoTime();
-    // remainingTime = remainingTime/1000000; // Convert to milliseconds
-    // if(remainingTime < 0) {
-    // remainingTime = 0;
-    // }
-    // Thread.sleep((long) remainingTime);
-
-    // nextDrawTime += drawInterval;
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // }
-    // }
-    // }
-    // Delta method is more accurate than sleep method, so we will use delta method to calculate the time
     public void run() {
-        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+
+        double drawInterval = 1000000000 / FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
+
         while (gameThread != null) {
+
             currentTime = System.nanoTime();
             delta += (currentTime - lastTime) / drawInterval;
             lastTime = currentTime;
+
             if (delta >= 1) {
-                // Update: update information such as character positions
                 update();
-                // Drew: draw the screen with the updated information
                 repaint();
                 delta--;
             }
@@ -113,34 +88,37 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-        
-        // Draw the tiles
+
+        // Draw tiles
         tileM.draw(g2);
-        // Draw the objects
-        for (int i = 0; i < obj.length; i++) {
-            if (obj[i] != null) {
-                obj[i].draw(g2, this);
+
+        // 🔴 CHANGE: foreach loop for Collection
+        for (SuperObject object : objList) {
+            if (object != null) {
+                object.draw(g2, this);
             }
         }
 
-        // Draw the player
+        // Draw player
         player.draw(g2);
 
-        // Draw the UI
+        // Draw UI
         ui.draw(g2);
 
         g2.dispose();
     }
+
     // Sound Methods
     public void playSound(int i) {
-        music.setFile(i); // Set the sound file to play
-        music.play(); // Play the sound
-        music.loop(); // Loop the sound continuously
+        music.setFile(i);
+        music.play();
+        music.loop();
     }
-    // Stop the sound
+
     public void stopSound() {
         music.stop();
     }
+
     public void playSE(int i) {
         sound.setFile(i);
         sound.play();
